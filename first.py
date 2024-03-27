bacc4d9268dff99ec3c38605d401e872fd93d58d
EC2 Default User <ec2-user@ip-172-31-44-102.ap-southeast-2.compute.internal>
2024-03-27 23:34:52 +0000

    Evidence of participation

---
+++ first.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def main():
+    num = int(input('enter a number: ')) 
+    seq = 1
+    for seq in range (num):
+        print(seq)
+
+  
+if __name__== '__main__':
+    main()
