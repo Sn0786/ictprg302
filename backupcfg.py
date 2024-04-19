d59c01b0c58a6125b9817942741bcd85fffcb62d
EC2 Default User <ec2-user@ip-172-31-35-165.ap-southeast-2.compute.internal>
2024-04-19 04:44:11 +0000

    19Apr

--- backupcfg.py
+++ backupcfg.py
@@ -2,3 +2,18 @@ jobs = {'job17': '/home/ec2-user/environment/ictprg302/file1.dat',
         'job18': '/home/ec2-user/environment/ictprg302/dir1'}
         
 dstPath = '/home/ec2-user/environment/ictprg302/backups'
+
+logPath = '/home/ec2-user/environment/ictprg302/backup.log'
+
+
+
+
+smtp = {"sender": "sohanajmi10@gmail.com",    # elasticemail.com verified sender
+        "recipient": "30024957@students.sunitafe.edu.au", # elasticemail.com verified recipient
+        "server": "smtp.elasticemail.com",      # elasticemail.com SMTP server
+        "port": 2525,                           # elasticemail.com SMTP port
+        "user": "sohanajmi10@gmail.com",      # elasticemail.com user
+        "password": "91CC70827BFA8E882B97E88B6FE55749B6D0"  # elasticemail.com password
+        
+}    
+
